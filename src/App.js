afd5972c040c6b39868fe6f9670af6fa2337b9eb
Kamilya Mukhammedova <kamilichkam@gmail.com>
2022-06-09 22:23:17 +0600

    #3 Создание массива INGREDIENTS и стэйта для ингредиентов

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+import meatImage from './assets/Meat.png';
+import cheeseImage from './assets/Cheese.png';
+import saladImage from './assets/Salad.png';
+import baconImage from './assets/Bacon.png';
+import {useState} from "react";
+
+const INGREDIENTS = [
+  {name: 'Meat', price: 50, image: meatImage},
+  {name: 'Cheese', price: 20, image: cheeseImage},
+  {name: 'Salad', price: 5, image: saladImage},
+  {name: 'Bacon', price: 30, image: baconImage},
+];
+
 const App = () => {
+  const [ingredients, setIngredients] = useState([
+    {name: 'Meat', count: 0},
+    {name: 'Cheese', count: 0},
+    {name: 'Salad', count: 0},
+    {name: 'Bacon', count: 0},
+  ]);
+
   return (
     <></>
   );
